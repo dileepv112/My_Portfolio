187ac6f3b414010ed46c2b493c802f403d586b49
dileep vishwakarma <211b112@juetguna.in>
2023-09-26 20:57:48 +0530

    Create react app

---
+++ client/src/components/HeroImg.js
@@ -0,0 +1,16 @@
+import "./HeroImgStyle.css";
+
+import React from 'react'
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default HeroImg
