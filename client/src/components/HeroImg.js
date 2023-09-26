9ead5a9efc1415fd9a51d5dc5cff390710550f2b
dileep vishwakarma <211b112@juetguna.in>
2023-09-26 22:05:14 +0530

    create header,navbar and hero section

--- client/src/components/HeroImg.js
+++ client/src/components/HeroImg.js
@@ -1,14 +1,26 @@
 import "./HeroImgStyle.css";
-
+import IntroImg from "../assets/introimg.jpg"
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
-
+           <img className="intro-img" src={IntroImg} alt="IntroImg"/>
         </div>
-      
+        <div className="content">
+          <p>HI, I'M A FREELANCER.</p>
+          <h1>Full Stack Developer</h1>
+          <div>
+            <Link to="/project" className="btn"> 
+               projects
+            </Link>
+            <Link to="/contact" className="btn btn-light"> 
+               contact
+            </Link>
+          </div>
+        </div>
     </div>
   )
 }
