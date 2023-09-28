0a0aa3326d06b22f737bd14039bfa4089a7d5649
dileep vishwakarma <211b112@juetguna.in>
2023-09-28 19:32:35 +0530

    styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,22 +10,22 @@ const Footer = () => {
                <div className="location">
                   <FaHome size={20} style={{color:"#fff", marginRight:"2rem"}} />
                   <div>
-                    <p>123 Housing Society.</p>
-                    <p>Bangladesh.</p>
+                    <p>Guna, Madhya Pradesh</p>
+                    <p>India</p>
                   </div>
                </div>
                <div className="phone">
                  <h4><FaPhone size={20} style={{color:"#fff", marginRight:"2rem"}} />
-                 123-342-544-3459</h4>
+                 +19-9302198620</h4>
                </div>
                <div className="email">
                  <h4><FaMailBulk size={20} style={{color:"#fff", marginRight:"2rem"}} />
-                 info@gmail.com</h4>
+                 dileepv10032002@gmail.com</h4>
                </div>
            </div>
            <div className="right">
-             <h4>About the company</h4>
-             <p>This is me fahimul kabir. CEO & Founder of Tech2etc.</p>
+             <h4>About Me</h4>
+             <p>I'M a Full Stack Developer. Currently I'M pursuing B.Tech from Jaypee University Of Engineering and Technology Guna MP.</p>
              <div className="social">
                 <FaFacebook size={30} style={{color:"#fff", marginRight:"1rem"}} />
                 <FaTwitter size={30} style={{color:"#fff", marginRight:"1rem"}} />
--- client/src/components/HeroImg.js
+++ client/src/components/HeroImg.js
@@ -2,6 +2,7 @@ import "./HeroImgStyle.css";
 import IntroImg from "../assets/introimg.jpg"
 import React from 'react'
 import {Link} from "react-router-dom"
+import Typical from "react-typical";
 
 const HeroImg = () => {
   return (
@@ -10,8 +11,22 @@ const HeroImg = () => {
            <img className="intro-img" src={IntroImg} alt="IntroImg"/>
         </div>
         <div className="content">
-          <p>HI, I'M A FREELANCER.</p>
-          <h1>Full Stack Developer</h1>
+          <p>HI, I'M Dileep Vishwakarma</p>
+          <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Full Stack Devloper💻",
+                    1000,
+                    "MERN Stack Dev😎",
+                    1000,
+                  ]}
+                />
+              </h1>
+             
+            </span>
           <div>
             <Link to="/project" className="btn"> 
                projects
@@ -19,6 +34,9 @@ const HeroImg = () => {
             <Link to="/contact" className="btn btn-light"> 
                contact
             </Link>
+            <a href="Resume.jpg" download="Dileep Resume.jpg">
+              <button className="btn">Get Resume</button>
+            </a>
           </div>
         </div>
     </div>
