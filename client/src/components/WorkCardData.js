d3177c3f28d23ce77515081436ef885d4408aee8
dileep vishwakarma <211b112@juetguna.in>
2023-09-28 10:46:06 +0530

    add project dynamic cards

---
+++ client/src/components/WorkCardData.js
@@ -0,0 +1,27 @@
+import pro1 from "../assets/project1.jpg"
+import pro2 from "../assets/project2.jpg"
+import pro3 from "../assets/project3.jpg"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "Thriving Facebook community...",
+        view: "#"
+    },
+    {
+        imgsrc: pro2,
+        title: "LMS/Course Website Design",
+        text: "Thriving Facebook community...",
+        view: "#"
+    },
+    {
+        imgsrc: pro3,
+        title: "Rymo Ecommerce Design",
+        text: "Thriving Facebook community...",
+        view: "#"
+    }
+    // if you want to add many cards edit here.
+];
+
+export default ProjectCardData;
